fe4b83991c0c4d515d32d6112a2a6fd4800c8ef3
Jacob Dart <jacobdart@gmail.com>
2025-12-18 15:43:35 +0000

    tidy up aoc2

--- day02/aoc02.R
+++ day02/aoc02.R
@@ -13,12 +13,13 @@ test_input <- read.csv(filepath,
                        header=F)
 
 num_ranges <- length(test_input)/2
+
+#part1
 running_total <- 0
 
 for (range_id in 1:num_ranges){
   for (value in test_input[1,(2*range_id-1)]:test_input[1,(2*range_id)]){
     if (str_detect(as.character(value),"(^[0-9]+)(?=\\1$)")){
-      print(value)
       running_total <- running_total+value
     }
   }
@@ -26,17 +27,12 @@ for (range_id in 1:num_ranges){
 
 running_total
 
-str_detect("121212","(^[0-9]+)(?=\\1+$)")
-
 #part 2
-
-
 running_total <- 0
 
 for (range_id in 1:num_ranges){
   for (value in test_input[1,(2*range_id-1)]:test_input[1,(2*range_id)]){
     if (str_detect(as.character(value),"(^[0-9]+)(?=\\1+$)")){
-      print(value)
       running_total <- running_total+value
     }
   }
